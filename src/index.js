13d3bd14b1e6af72a0f8193c1bb81485507c8783
findsakshat <voteforsakshat@gmail.com>
2024-04-06 02:01:11 +0530

    feat: handled copying images from public to dist

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 function renderUI() {
   const root = document.getElementById("root");
-  root.innerHTML = "<h1>Hello from Webpack</h1>";
+
+  // HEADING
+  const heading = document.createElement("h1");
+  heading.innerText = "Hello from Webpack 1234"
+
+  // IMAGE 
+  const image = document.createElement("img");
+  image.setAttribute("src", "/dog.jpeg");
+  image.setAttribute("alt", "vodafone-doggy");
+  image.setAttribute("height", "300");
+  image.setAttribute("width", "300");
+
+  image.style.border = "1px solid #eee",
+  image.style.borderRadius = "8px";
+
+  // APPEND TO THE ROOT
+  root.appendChild(heading);
+  root.appendChild(image);
 }
 
 renderUI();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,10 +10,18 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html", // Path to HTML template 
+      template: "./index.html", // Path to HTML template 
       filename: 'index.html', // Output HTML filename
       inject: 'body', // Inject the script tag into the body of the HTML file
     }),
+    new CopyWebpackPlugin({ // Copy the 'public' directory to the 'dist' directory
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public'), // Source directory to copy from
+          to: path.resolve(__dirname, 'dist') // Destination directory to copy to
+        }
+      ]
+    })
   ],
   devServer: {
     static: {
